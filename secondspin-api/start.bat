76358d3974223555c7af3846622fb7937f3c7724
ld <2636064351@qq.com>
2025-06-08 19:35:27 +0800

    update starting script

--- secondspin-api/start.bat
+++ secondspin-api/start.bat
@@ -2,10 +2,12 @@
 
 setlocal enabledelayedexpansion
 
+set YOUR_DOCKER_SERVER_IP=39.108.112.191
+
 set SERVICES=gateway user-service product-service order-service payment-service message-service
 
 for %%s in (%SERVICES%) do (
-    start "%%s" cmd /c "cd %%s && mvn spring-boot:run"
+    start "%%s" cmd /c "mvn -pl %%s spring-boot:run"
     timeout /t 5 >nul
 )
 
